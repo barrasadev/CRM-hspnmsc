cf5eb95bc02bd6144429d3dcb1ac78c53d9957e0
IvanGB <clase@barrasa.dev>
2025-02-14 14:14:42 +0100

    feat: afiliados artistas

--- odoo/addons/igb_hispanmusic/models/artista.py
+++ odoo/addons/igb_hispanmusic/models/artista.py
@@ -9,8 +9,12 @@ class Artista(models.Model):
     foto = fields.Image(string='Foto', required=True)
     telefono = fields.Char(string='Teléfono')
     email = fields.Char(string='Email')
+
     canciones_ids = fields.Many2many('igb_hispanmusic.cancion', string='Canciones')
     spotify = fields.Char(string='Spotify')
     youtube = fields.Char(string='YouTube')
     instagram = fields.Char(string='Instagram')
     applemusic = fields.Char(string='Apple Music')
+
+    referido_por_id = fields.Many2one('igb_hispanmusic.artista', string='Referido por', domain="[('id', '!=', id)]", ondelete='set null')
+    saldo_afiliado = fields.Float(string='Saldo Afiliado', default=0.00, digits=(10,2), help="Saldo acumulado por el artista como afiliado", readonly=False)
