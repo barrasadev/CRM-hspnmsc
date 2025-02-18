6a275084ee2f09e5277c95512330bc87364aa5f0
IvanGB <clase@barrasa.dev>
2025-02-18 13:56:57 +0100

    feat: Give money to afiliate

--- odoo/addons/igb_hispanmusic/models/artistas/transaccion.py
+++ odoo/addons/igb_hispanmusic/models/artistas/transaccion.py
@@ -1,9 +1,3 @@
-# Author: Ivan Gonzalez Barrasa (barrasa.dev)
-# File: transaccion.py
-# Path: ~/Proyectos/HispanMusic/CRM-hspnmsc/odoo/addons/igb_hispanmusic/models/artistas
-# Created: 2025-02-17 13:04
-# Last Updated: 2025-02-17 13:04
-
 from odoo import models, fields, api
 
 class Transaccion(models.Model):
@@ -29,12 +23,26 @@ class Transaccion(models.Model):
 
     @api.model
     def create(self, vals):
-        """ Sobreescribe create para actualizar el saldo del artista """
+        """ Sobreescribe create para actualizar el saldo del artista y manejar referidos """
         transaccion = super(Transaccion, self).create(vals)
 
         if transaccion.tipo_transaccion == 'ingreso':
             transaccion.artista_id.saldo += transaccion.importe
         elif transaccion.tipo_transaccion == 'retiro':
             transaccion.artista_id.saldo -= transaccion.importe
+
+            # Verificar si el mensaje comienza con '[PLAYLISTS'
+            if transaccion.mensaje.startswith('[PLAYLISTS'):
+                artista_referido = transaccion.artista_id.referido_por_id
+                if artista_referido:
+                    importe_referido = transaccion.importe * 0.15
+                    mensaje_referido = f'REFERIDO ARTISTA #{transaccion.artista_id.id} TRANSACCION #{transaccion.id}'
+
+                    self.create({
+                        'artista_id': artista_referido.id,
+                        'tipo_transaccion': 'ingreso',
+                        'importe': importe_referido,
+                        'mensaje': mensaje_referido,
+                    })
 
         return transaccion
